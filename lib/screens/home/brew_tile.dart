65bb5c271ad8d59c6d78380cad732e4791760fa6
Wenyi Liu <wenyiliu95@gmail.com>
2021-08-24 01:19:58 -0400

    Update home body

--- lib/screens/home/brew_tile.dart
+++ lib/screens/home/brew_tile.dart
@@ -7,6 +7,19 @@ class BrewTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.brown[brew.strength],
+          ),
+          title: Text(brew.name),
+          subtitle: Text('Takes ${brew.sugar} sugar(s)'),
+        ),
+      ),
+    );
   }
 }
